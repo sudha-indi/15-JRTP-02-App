7feda49f4d4170716efdf8c367f54c0dd7fab74d
sudha <sudha.indi11@gmail.com>
2021-05-21 17:43:43 +0530

    My First commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+package in.ashokit.bean;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class PwdEncoder {
+	public static String encode(String pwd)
+	{
+		Encoder encoder=Base64.getEncoder();
+		String encodedString=encoder.encodeToString(pwd.getBytes());
+		return encodedString;
+	}
+
+}
